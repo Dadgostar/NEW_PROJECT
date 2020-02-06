443981a75774f4c67aea6391393534cf7f20b7e7
pouyamsadeghi <37483481+pouyamsadeghi@users.noreply.github.com>
2020-02-06 04:32:41 -0800

    Student_professor_evaluation added

---
+++ AP_PROJECT/AP_PROJECT/View/Student_professor_evaluation.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AP_PROJECT.View
+{
+    /// <summary>
+    /// Interaction logic for Student_professor_evaluation.xaml
+    /// </summary>
+    public partial class Student_professor_evaluation : Window
+    {
+        public Student_professor_evaluation()
+        {
+            InitializeComponent();
+        }
+    }
+}
