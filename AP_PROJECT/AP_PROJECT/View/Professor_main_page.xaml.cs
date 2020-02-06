aa407d5b3f23771aa120efa6204496946fb3cfbe
pouyamsadeghi <37483481+pouyamsadeghi@users.noreply.github.com>
2020-02-06 06:07:25 -0800

    professor main page added

---
+++ AP_PROJECT/AP_PROJECT/View/Professor_main_page.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AP_PROJECT.View
+{
+    /// <summary>
+    /// Interaction logic for Professor_main_page.xaml
+    /// </summary>
+    public partial class Professor_main_page : Window
+    {
+        public Professor_main_page()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
