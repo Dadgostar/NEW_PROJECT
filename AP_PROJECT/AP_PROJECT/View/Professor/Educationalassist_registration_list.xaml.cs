1cff35f7c2dd331daf66f5f506a820711edf79fb
pouyamsadeghi <37483481+pouyamsadeghi@users.noreply.github.com>
2020-02-06 07:56:26 -0800

    change by me :))

---
+++ AP_PROJECT/AP_PROJECT/View/Professor/Educationalassist_registration_list.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AP_PROJECT.View.Professor
+{
+    /// <summary>
+    /// Interaction logic for Educationalassist_registration_list.xaml
+    /// </summary>
+    public partial class Educationalassist_registration_list : Window
+    {
+        public Educationalassist_registration_list()
+        {
+            InitializeComponent();
+        }
+    }
+}
