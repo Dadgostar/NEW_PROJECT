a8b069d5ddec41d5f1669c44d26e512fd0adfc0a
Dadgostar <mahed98@gmail.com>
2020-02-07 20:16:48 +0330

    offered courses changed

--- AP_PROJECT/AP_PROJECT/View/Student/Student_main_page.xaml.cs
+++ AP_PROJECT/AP_PROJECT/View/Student/Student_main_page.xaml.cs
@@ -51,8 +51,8 @@ namespace AP_PROJECT.View
 
         private void WeeklyScheduleClicked(object sender, RoutedEventArgs e)
         {
-            WeekSchedule weekSchedual = new WeekSchedule();
-            weekSchedual.Show();
+            WeekSchedule weekschedule = new WeekSchedule(student);
+            weekschedule.Show();
         }
 
         private void ExcepForm(object sender, RoutedEventArgs e)
@@ -76,6 +76,7 @@ namespace AP_PROJECT.View
         private void PaymentClicked(object sender, RoutedEventArgs e)
         {
             double payment = Module.GetPayment(this.student);
+            PaymentLable.Content =  "" + payment ;
                 
         }
 
